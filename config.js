5ff6f6bafbd5e423c9b42a07f90d8128f315ae3b
AkiBOTS <119124747+AkiBOTS@users.noreply.github.com>
2022-11-27 15:01:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 global.you = ['6283863727744'] 
 //Developer Bot
 global.gc = 'https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp'
-global.rowner = ['6283863727744'] // Real Owner Bot
-global.owner = ['6283863727744'] // Owner Bot
+global.rowner = ['6281343243912'] // Real Owner Bot
+global.owner = ['6281343243912'] // Owner Bot
 global.police = ['6283863727744'] // Police Bot
-global.mods = ['6283863727744']  // Want some help?
-global.prems = ['6283863727744'] // Premium user has unlimited limit
+global.mods = ['6281343243912']  // Want some help?
+global.prems = ['6281343243912'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini juga ganti
@@ -83,9 +83,9 @@ global.fsizedoc = '564588643' // default 10TB
 global.fpagedoc = '444'
 
 // Sticker WM
-global.packname = 'Yuni-MD'
-global.author = '©ᵈʳкσкσ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
-global.wm = 'Yuni-MD | ©ᵈʳкσкσ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
+global.packname = 'Aki-MD'
+global.author = '©ᵈʳAki ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
+global.wm = 'Aki-MD | 62813432439122'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
